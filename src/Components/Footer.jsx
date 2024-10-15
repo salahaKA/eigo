a0b5387b8e22a5a45e63bc11bd5bec7b75272aa7
Kadeejath Salaha <codestudy475@gmail.com>
2024-10-15 14:01:40 +0530

    All done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,33 +1,42 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faGithub,
+  faBlogger,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <footer>
       <p className="footer-links">
       <a href="https://www.instagram.com/eigo_shopping/" target="_blank">
-          Instagram
+      <FontAwesomeIcon icon={faInstagram} /> Instagram
+        </a>
+        
+        <span> / </span>
+        <a href="mailto:codestudy475@gmail.com" target="_blank">
+        <FontAwesomeIcon icon={faEnvelope} /> Need any help?
+        </a>
+        
+        
+        <span> / </span>
+        <a href="https://www.instagram.com/eigo_shopping/" target="_blank">
+        <FontAwesomeIcon icon={faBlogger} /> Read My Blog
+        </a>
+      </p>
+      <p>
+        &copy; {currentYear} <strong>Eigo</strong> - Online Shopping Store - Designed by Kadeejath Salaha @codestudy4 <a href="https://www.instagram.com/code_study4/" target="_blank">
+      <FontAwesomeIcon icon={faInstagram} /> Instagram
         </a>
         <span> / </span>
         <a
           href="https://github.com/salahaKA/eigo_shoppingweb.git"
           target="_blank"
         >
-          View Source on Github
+          <FontAwesomeIcon icon={faGithub} /> View Source on Github
         </a>
-        <span> / </span>
-        <a href="mailto:codestudy475@gmail.com" target="_blank">
-          Need any help?
-        </a>
-        
-        
-        <span> / </span>
-        <a href="https://www.instagram.com/eigo_shopping/" target="_blank">
-          Read My Blog
-        </a>
-      </p>
-      <p>
-        &copy; {currentYear} <strong>Eigo</strong> - Online Shopping Store - Designed by Kadeejath Salaha
       </p>
     </footer>
   );
